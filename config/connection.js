65d7eba6bc6cc0f073b233ed0c6dd4bb24d7ffbd
muneebmemon <muneeb.memon@gmail.com>
2018-03-06 18:45:10 -0600

    Added JAWSDB functionality for Heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 // Set up MySQL connection.
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-  port: 3360,
-  host: "localhost",
-  user: "root",
-  password: "mypassword1927",
-  database: "burgers_db"
-});
+var connection;
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+   port: 3360,
+   host: "localhost",
+   user: "root",
+   password: "",
+   database: "burgers_db"
+  });
+}
 // Making connection
 connection.connect(function(err) {
   if (err) {
